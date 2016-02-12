a266edf32f6b1e4a7c545cae20d9af11c0da79c0
Justin Bitely <jbitely@gmail.com>
2016-02-12 18:43:26 -0500

    Implementeed ShortenController.

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -1,5 +1,15 @@
 angular.module('shortly.shorten', [])
 
-.controller('ShortenController', function ($scope, $location, Links) {
+.controller('ShortenController', function ($scope, $location, Links, $http) {
   // Your code here
+  $scope.link = {}; // may need refactor for actual solution
+
+  $scope.addLink = function () {
+    $http({
+      method: 'POST',
+      url: '/api/links',
+      data: JSON.stringify($scope.link)
+    })
+  };
+
 });
