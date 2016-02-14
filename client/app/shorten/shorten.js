88b5146f9dd598c68d7a2595a1c4749da84d3420
Justin Bitely <jbitely@gmail.com>
2016-02-13 20:20:54 -0500

    Ended pairing. ngFx dependency issues, reverted to ngAnimate for now.

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -33,9 +33,7 @@ angular.module('shortly.shorten', [])
   return function (scope, el, attr) {
 
     urlRegex = /^(?!mailto:)(?:(?:https?|ftp):\/\/)?(?:\S+(?::\S*)?@)?(?:(?:(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[0-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))|localhost)(?::\d{2,5})?(?:\/[^\s]*)?$/i;
-    console.log('regex: ', urlRegex);
-    console.log('regex: ', typeof urlRegex);
-    el.on('keyup', function () {
+    el.on('change', function () {
 
       if (!el.val() || urlRegex.test(el.val())) {
         el.removeClass('error');
